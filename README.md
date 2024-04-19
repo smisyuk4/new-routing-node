391cea156ecf5b55274ea5922d7bec50a403c123
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-04-20 00:13:47 +0300

    write CRUD operations for SQLite

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## data base
 
 - [sqlite - nodejs](https://www.sqlitetutorial.net/sqlite-nodejs/connect/)
-# new-routing-node
+- [video-1](https://www.youtube.com/watch?v=ZRYn6tgnEgM&ab_channel=ByteMyke)
+- [Build a Node JS SQLite API tutorial - video](https://www.youtube.com/watch?v=mnH_1YGR2PM&ab_channel=ByteMyke)
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-//const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
+let sql;
 const app = express();
 const port = 3000;
 
@@ -15,9 +16,60 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-//let db = new sqlite3.Database(':memory:', (err) => {
+// connect to db
+let db = new sqlite3.Database('./posts.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+// create table
+//sql = `CREATE TABLE posts(id INTEGER PRIMARY KEY,title,message)`;
+//db.run(sql);
+
+// drop table
+//db.run('DROP TABLE posts');
+
+// insert data into table
+//sql = `INSERT INTO posts(title, message) VALUES(?,?)`;
+//db.run(sql, ['Angular', 'fine tool for development power SPA'], (err) => {
 //  if (err) {
 //    return console.error(err.message);
 //  }
-//  console.log('Connected to the in-memory SQlite database.');
 //});
+
+// update data
+//sql = `UPDATE posts SET title = ? WHERE id = ?`;
+//db.run(sql, [
+//  'Next.js',
+//  1],
+//  (err) => {
+//    if (err) {
+//      return console.error(err.message);
+//    }
+//  },
+//);
+
+// delete data
+//sql = `DELETE FROM posts WHERE id = ?`;
+//db.run(sql, [
+//  1,
+//  (err) => {
+//    if (err) {
+//      return console.error(err.message);
+//    }
+//  },
+//]);
+
+// query the data
+sql = `SELECT * FROM posts`;
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
