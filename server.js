4d8d334d15556172bf30f5917a99420bbfd061b6
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-04-20 18:29:39 +0300

    write CRUD: update, delete routes

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const url = require('url');
+require('dotenv').config();
 
 let sql;
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 
 // connect to db
 let db = new sqlite3.Database('./posts.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -17,6 +18,14 @@ let db = new sqlite3.Database('./posts.db', sqlite3.OPEN_READWRITE, (err) => {
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+  return res.json({
+    status: 200,
+    success: true,
+    text: 'hello world',
+  });
+});
 
 app.get('/posts', async (req, res) => {
   sql = `SELECT * FROM posts`; // get all data
@@ -62,11 +71,18 @@ app.get('/posts', async (req, res) => {
 
 app.post('/create-post', async (req, res) => {
   const { title, message } = req.body;
+  sql = `INSERT INTO posts(title, message) VALUES(?,?)`;
+
+  if (!title || !message) {
+    return res.json({
+      status: 400,
+      success: false,
+      message: 'title and message required',
+    });
+  }
 
   try {
-    // insert data into table
-    sql = `INSERT INTO posts(title, message) VALUES(?,?)`;
-    db.run(sql, ['Angular', 'fine tool for development power SPA'], (err) => {
+    db.run(sql, [title, message], (err) => {
       if (err) {
         return res.json({
           status: 300,
@@ -75,7 +91,49 @@ app.post('/create-post', async (req, res) => {
         });
       }
 
-      console.log(req.body);
+      res.json({
+        status: 200,
+        success: true,
+      });
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      success: false,
+    });
+  }
+});
+
+app.patch('/update-post', async (req, res) => {
+  const { id, title, message } = req.body;
+  sql = 'UPDATE posts SET';
+  const params = [];
+
+  if (title) {
+    sql += ' title = ?,';
+    params.push(title);
+  }
+
+  if (message) {
+    sql += ' message = ?,';
+    params.push(message);
+  }
+
+  // remove last coma
+  sql = sql.slice(0, -1);
+  sql += ' WHERE id = ?';
+  params.push(id);
+
+  try {
+    //generated query as `UPDATE posts SET title = ?, message = ? WHERE id = ?`;
+    db.run(sql, params, (err) => {
+      if (err) {
+        return res.json({
+          status: 300,
+          success: false,
+          error: err,
+        });
+      }
     });
 
     res.json({
@@ -90,29 +148,33 @@ app.post('/create-post', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.delete('/delete-post', async (req, res) => {
+  const { id } = req.body;
+  sql = `DELETE FROM posts WHERE id = ?`;
+
+  try {
+    db.run(sql, [id], (err) => {
+      if (err) {
+        return res.json({
+          status: 300,
+          success: false,
+          error: err,
+        });
+      }
+
+      res.json({
+        status: 200,
+        success: true,
+      });
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      success: false,
+    });
+  }
 });
 
-// update data
-//sql = `UPDATE posts SET title = ? WHERE id = ?`;
-//db.run(sql, [
-//  'Next.js',
-//  1],
-//  (err) => {
-//    if (err) {
-//      return console.error(err.message);
-//    }
-//  },
-//);
-
-// delete data
-//sql = `DELETE FROM posts WHERE id = ?`;
-//db.run(sql, [
-//  1,
-//  (err) => {
-//    if (err) {
-//      return console.error(err.message);
-//    }
-//  },
-//]);
+app.listen(PORT, () => {
+  console.log(`My server started on port ${PORT}`);
+});
