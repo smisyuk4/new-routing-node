1d4b7eaefa67f5de31060acc38c3a1a84942a38c
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-04-23 12:42:37 +0300

    write some swagger poutes

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -5,26 +5,49 @@
  *     Authors:
  *       type: object
  *       properties:
- *         id:
+ *         author_id:
  *           type: integer
- *           description: The auto-generated id of the post
- *         title:
+ *           description: The auto-generated id of the author
+ *         name:
  *           type: string
- *           description: The title of your post
- *         message:
+ *           description: The name of author
+ *         email:
  *           type: string
- *           description: The message of your post
+ *           description: The email of author
+ *         location:
+ *           type: sting
+ *           description: The location of author
+ *         avatar_url:
+ *           type: sting
+ *           description: The avatar url of author
+ *         token:
+ *           type: sting
+ *           description: The refreshToken of author
+ *         date_publish:
+ *           type: sting
+ *           description: The date register of author
+ *         date_update:
+ *           type: sting
+ *           description: The date update profile author
+ *       required:
+ *         - name
+ *         - email
  *       example:
- *          id: 1
- *          title: React
- *          message: Good tool for dev SPA
+ *          author_id: 32
+ *          name: Mary Grabovski
+ *          email: mary_gra@gmail.com
+ *          location: Spain, Madrid
+ *          avatar_url: https://static.vecteezy.com/system/resources/thumbnails/022/714/697/small/cute-black-and-white-girl-posing-vector.jpg
+ *          token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImVtYWlsIjoibW1AbW0uY29tIiwiaWF0IjoxNzEzODE5NjQ3fQ.saeTL2QhLu5HrurMTDy5xLL1uCmaLWa31fZonWyxnLU
+ *          date_publish: 2024-04-22T21:00:47.748Z
+ *          date_update: 2024-04-22T22:05:57.255Z
  */
 
 /**
  * @swagger
  * tags:
- *   name: Posts
- *   description: The posts managing API
+ *   name: Authors
+ *   description: The authors managing API
  */
 
 const express = require('express');
@@ -44,10 +67,10 @@ const { authenticationToken } = require('../middleware/authenticationToken');
 
 /**
  * @swagger
- * /create-post:
+ * /register:
  *   post:
- *     summary: Create a new post
- *     tags: [Posts]
+ *     summary: Create a new author
+ *     tags: [Authors]
  *     requestBody:
  *       required: true
  *       content:
@@ -55,12 +78,12 @@ const { authenticationToken } = require('../middleware/authenticationToken');
  *           schema:
  *             type: object
  *             required:
- *               - title
- *               - message
+ *               - name
+ *               - email
  *             properties:
- *               title:
+ *               name:
  *                 type: string
- *               message:
+ *               email:
  *                 type: string
  *     responses:
  *       200:
@@ -68,14 +91,115 @@ const { authenticationToken } = require('../middleware/authenticationToken');
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/Posts'
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *       400:
+ *         description: Name and email required or other errors.
+ *       409:
+ *         description: Email already exists.
  */
 router.post('/register', asyncWrapper(registerAuthor));
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Authentication author
+ *     tags: [Authors]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The created post.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *       400:
+ *         description: Name and email required or other errors.
+ */
 router.post('/login', asyncWrapper(loginAuthor));
 
+/**
+ * @swagger
+ * /token:
+ *   post:
+ *     summary: Refresh accessToken
+ *     tags: [Authors]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: AccessToken generated.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *       401:
+ *         description: Error.
+ *       403:
+ *         description: Not authorized.
+ */
 router.post('/token', asyncWrapper(checkAndGenerateToken));
 
+/**
+ * @swagger
+ * /logout:
+ *   post:
+ *     summary: Logout author
+ *     tags: [Authors]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Logout successfully.
+ *       400:
+ *         description: RefreshToken is required or other errors.
+ *       401:
+ *         description: Not authorized.
+ */
 router.post('/logout', asyncWrapper(logOutAuthor));
 
 /**
