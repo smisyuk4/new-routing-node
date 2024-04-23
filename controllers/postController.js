c51cad11ea8ccaf3f2c068f32d3a62153812a055
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-04-23 16:28:41 +0300

    refactor post services and controllers

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,10 @@
+const {
+  addPost,
+  updateFieldsPost,
+  getAllPosts,
+  getPostsByQuery,
+} = require('../services/postServices');
+
 const url = require('url');
 const sqlite3 = require('sqlite3').verbose();
 
@@ -16,159 +23,66 @@ const db = new sqlite3.Database(
 
 const createPost = async (req, res) => {
   const { author_id, title, message } = req.body;
-  const dateISO = new Date().toISOString();
-  sql = `INSERT INTO posts(author_id, title, message, date_publish, date_update) VALUES(?,?,?,?,?)`;
 
-  if (!title || !message || !author_id) {
+  if (!author_id || !title || !message) {
     return res.status(400).json({
-      success: false,
       message: 'author_id, title and message required',
     });
   }
 
   try {
-    db.run(sql, [author_id, title, message, dateISO, dateISO], (err) => {
-      if (err) {
-        return res.status(300).json({
-          success: false,
-          error: err,
-        });
-      }
+    const result = await addPost(author_id, title, message);
 
-      res.status(200).json({
-        success: true,
-      });
-    });
+    if (result?.status) {
+      return res.sendStatus(204);
+    }
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-    });
+    return res.status(400).json(error);
   }
 };
 
 const updatePost = async (req, res) => {
   const { post_id, title, message } = req.body;
-  const dateISO = new Date().toISOString();
-  sql = 'UPDATE posts SET';
-  const params = [];
 
   if (!post_id) {
     return res.status(400).json({
-      success: false,
       message: 'post_id is required',
     });
   }
 
-  if (title) {
-    sql += ' title = ?,';
-    params.push(title);
-  }
-
-  if (message) {
-    sql += ' message = ?,';
-    params.push(message);
-  }
-
-  sql += ' date_update = ? WHERE post_id = ?';
-  params.push(dateISO);
-  params.push(post_id);
-
   try {
-    //generated query as `UPDATE posts SET title = ?, message = ?, date_update = ? WHERE post_id = ?`;
-    db.run(sql, params, function (err) {
-      if (err) {
-        return res.status(300).json({
-          success: false,
-          error: err,
-        });
-      }
-
-      if (this.changes === 0) {
-        return res.status(300).json({
-          success: false,
-          message: `Post with id: ${post_id} not found.`,
-        });
-      }
-
-      res.status(200).json({
-        success: true,
-      });
-    });
+    const result = await updateFieldsPost(post_id, title, message);
+    if (result?.status) {
+      return res.sendStatus(204);
+    }
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-    });
+    return res.status(400).json(error);
   }
 };
 
-const getAllPosts = async (req, res) => {
-  sql = `SELECT * FROM posts`;
-
+const getPosts = async (req, res) => {
   try {
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        return res.status(300).json({
-          success: false,
-          error: err,
-        });
-      }
-
-      if (rows.length < 1) {
-        return res.status(300).json({
-          success: false,
-          error: 'No match posts',
-        });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: rows,
-      });
-    });
+    const result = await getAllPosts();
+    if (result?.length > 0) {
+      return res.status(200).json({ posts: result });
+    }
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-    });
+    return res.status(400).json(error);
   }
 };
 
 const getFilteredPosts = async (req, res) => {
   const { author_id } = req.author;
-  const queryObject = url.parse(req.url, true).query;
-
-  sql = `SELECT * FROM posts WHERE`;
-
-  if (queryObject.field && queryObject.value) {
-    sql += ` ${queryObject.field} LIKE '%${queryObject.value}%' AND`;
-  }
-
-  sql += ` author_id = ?`;
+  const { field, value } = url.parse(req.url, true).query;
 
   try {
-    db.all(sql, [author_id], (err, rows) => {
-      if (err) {
-        return res.status(300).json({
-          success: false,
-          error: err,
-        });
-      }
+    const result = await getPostsByQuery(author_id, field, value);
 
-      if (rows.length < 1) {
-        return res.status(300).json({
-          success: false,
-          error: 'No match posts',
-        });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: rows,
-      });
-    });
+    if (result?.length > 0) {
+      return res.status(200).json({ posts: result });
+    }
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-    });
+    return res.status(400).json(error);
   }
 };
 
@@ -197,7 +111,7 @@ const deletePost = async (req, res) => {
 };
 
 module.exports = {
-  getAllPosts,
+  getPosts,
   getFilteredPosts,
   createPost,
   updatePost,
---
+++ services/postServices.js
@@ -0,0 +1,110 @@
+const sqlite3 = require('sqlite3').verbose();
+const { constants } = require('../constants');
+
+// connect to db
+let sql;
+const dateISO = new Date().toISOString();
+const db = new sqlite3.Database(
+  './posts-new.db',
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  }
+);
+
+const addPost = (author_id, title, message) => {
+  return new Promise((resolve, reject) => {
+    sql = `INSERT INTO posts(author_id, title, message, date_publish, date_update) VALUES(?,?,?,?,?)`;
+
+    return db.run(sql, [author_id, title, message, dateISO, dateISO], (err) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve({ status: true });
+    });
+  });
+};
+
+const updateFieldsPost = (post_id, title, message) => {
+  return new Promise((resolve, reject) => {
+    sql = 'UPDATE posts SET';
+    const params = [];
+
+    if (title) {
+      sql += ' title = ?,';
+      params.push(title);
+    }
+
+    if (message) {
+      sql += ' message = ?,';
+      params.push(message);
+    }
+
+    sql += ' date_update = ? WHERE post_id = ?';
+    params.push(dateISO);
+    params.push(post_id);
+
+    return db.run(sql, params, function (err) {
+      if (err) {
+        return reject(err);
+      }
+
+      if (this.changes === 0) {
+        reject({ message: `Post with id: ${post_id} not found.` });
+      }
+
+      return resolve(rows);
+    });
+  });
+};
+
+const getAllPosts = () => {
+  return new Promise((resolve, reject) => {
+    sql = `SELECT * FROM posts`;
+
+    return db.all(sql, [], (err, rows) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (rows.length < 1) {
+        return reject({ message: 'No match posts' });
+      }
+
+      return resolve(rows);
+    });
+  });
+};
+
+const getPostsByQuery = (author_id, field, value) => {
+  return new Promise((resolve, reject) => {
+    sql = `SELECT * FROM posts WHERE`;
+    if (field && value) {
+      sql += ` ${field} LIKE '%${value}%' AND`;
+    }
+    sql += ` author_id = ?`;
+
+    return db.all(sql, [author_id], (err, rows) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (rows.length < 1) {
+        return reject({ message: 'No match posts' });
+      }
+
+      return resolve(rows);
+    });
+  });
+};
+
+module.exports = {
+  addPost,
+  updateFieldsPost,
+  getAllPosts,
+  getPostsByQuery,
+};
