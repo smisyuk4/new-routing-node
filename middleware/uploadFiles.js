01d5d62d1ed63d2c579e14148c17abf523e850b8
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-04-29 18:55:26 +0300

    write func for control mime type in multer

--- middleware/uploadFiles.js
+++ middleware/uploadFiles.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const { constants } = require('../constants');
 
 const MAX_FILE_SIZE = 3;
 
@@ -7,9 +8,23 @@ const multerMid = multer({
   limits: {
     fileSize: MAX_FILE_SIZE * 1024 * 1024,
   },
+  fileFilter: function (req, file, cb) {
+    if (
+      file.mimetype !== 'image/png' &&
+      file.mimetype !== 'image/jpeg' &&
+      file.mimetype !== 'image/webp'
+    ) {
+      return cb(constants.IMAGE_TYPE, false);
+    }
+    cb(null, true);
+  },
 });
 
 const multerErrorHandling = (err, req, res, next) => {
+  if (err === constants.IMAGE_TYPE) {
+    return res.status(400).send('Multer error: ' + constants.IMAGE_TYPE);
+  }
+
   if (err instanceof multer.MulterError) {
     if (err.code === 'LIMIT_FILE_SIZE') {
       return res
