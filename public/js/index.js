2eb7e66e30541fdb41b1219c020a479b1ee8af74
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-05-02 00:21:07 +0300

    write scroll-up button

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,9 @@
     burgerMenuBtn: document.querySelector('.burger__menu'),
     burgerCloseBtn: document.querySelector('.burger__close'),
     burgerMenu: document.querySelector('.burger__modal'),
-
     mainContent: document.querySelector('main'),
     burgerMenuLinks: document.querySelectorAll('.burger__modal-link'),
+    scrollUpBtn: document.querySelector('.scroll-up'),
   };
 
   const toggleBurgerMenu = () => {
@@ -19,49 +19,20 @@
   refs.burgerMenuBtn.addEventListener('click', toggleBurgerMenu);
   refs.burgerCloseBtn.addEventListener('click', toggleBurgerMenu);
   refs.mainContent.addEventListener('click', handleCloseBurgerMenu);
-
   refs.burgerMenuLinks.forEach((link) => {
     link.addEventListener('click', handleCloseBurgerMenu);
   });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      refs.scrollUpBtn.classList.add('is-show');
+    } else {
+      refs.scrollUpBtn.classList.remove('is-show');
+    }
+  });
+
+  refs.scrollUpBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 })();
-
-//const btn = $('#up');
-
-//$(window).scroll(function () {
-//  if ($(window).scrollTop() > 300) {
-//    btn.addClass('show');
-//  } else {
-//    btn.removeClass('show');
-//  }
-//});
-
-//btn.on('click', function (e) {
-//  e.preventDefault();
-//  $('html, body').animate({ scrollTop: 0 }, '300');
-//});
-
-//{/*<a id="up"></a>*/}
-
-//window.addEventListener('DOMContentLoaded', () => {
-//  const refs = {
-//    burgerMenuBtn: document.querySelector('.burger__menu'),
-//    burgerCloseBtn: document.querySelector('.burger__close'),
-//    burgerMenu: document.querySelector('.burger__modal'),
-//    mainContent: document.querySelector('main'),
-//    burgerMenuLink: document.querySelector('#link'),
-//  };
-
-//  const toggleBurgerMenu = () => {
-//    refs.burgerMenu.classList.toggle('is-open');
-//  };
-
-//  const handleCloseBurgerMenu = () => {
-//    refs.burgerMenu.classList.remove('is-open');
-//    console.log('click main');
-//  };
-
-//  refs.burgerMenuBtn.addEventListener('click', toggleBurgerMenu);
-//  refs.burgerCloseBtn.addEventListener('click', toggleBurgerMenu);
-//  refs.mainContent.addEventListener('click', handleCloseBurgerMenu);
-//  refs.burgerMenuLink.addEventListener('click', handleCloseBurgerMenu);
-//});
